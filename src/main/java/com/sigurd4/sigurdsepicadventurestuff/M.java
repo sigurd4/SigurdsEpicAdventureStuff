b4cb286512563950402ef69c90ea4f4bade95a8e
sigurd4 <sigurd-s@online.no>
2015-03-20 16:33:25 +0100

    accidently imported this, but i dont wanna bother with removing it now
    busy busy busy

--- src/main/java/com/sigurd4/sigurdsepicadventurestuff/M.java
+++ src/main/java/com/sigurd4/sigurdsepicadventurestuff/M.java
@@ -27,6 +27,7 @@ import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 import net.minecraftforge.fml.common.registry.EntityRegistry;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
