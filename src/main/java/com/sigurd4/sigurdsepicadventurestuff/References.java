16b06da5a599a80e6e61287ad76f8031c1573e06
sigurd4 <sigurd-s@online.no>
2015-05-23 01:43:24 +0200

    patch 1.0.1 complete

--- src/main/java/com/sigurd4/sigurdsepicadventurestuff/References.java
+++ src/main/java/com/sigurd4/sigurdsepicadventurestuff/References.java
@@ -8,19 +8,8 @@ public class References
 {
 	public static final String MODID = "sigurdsEpicAdventureStuff";
 	public static final String NAME = "Sigurd's Epic Adventure Stuff";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.0.1";
 	public static final String CLIENT_PROXY_CLASS = "com.sigurd4.sigurdsEpicAdventureStuff.proxy.ProxyClient";
 	public static final String SERVER_PROXY_CLASS = "com.sigurd4.sigurdsEpicAdventureStuff.proxy.ProxyServer";
 	public static final String GUI_FACTORY_CLASS = "com.sigurd4.sigurdsEpicAdventureStuff.gui.GuiFactory";
-	
-/*
-CHANGELOG:
-1.0 - sigurd4:
-+mystery potions!
-+special swords!
-  •adventure sword
-  •sky sword
-+creative tab
-+config
-*/
 }
